12c0537ba0ecceec11800571005d93956dbf216d
Winiicius <winiciusalexandre@gmail.com>
2026-02-08 23:22:02 -0300

    chore: add Chakra UI

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Provider } from './components/ui/provider'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
+    <Provider>
+
+    </Provider>
   </StrictMode>,
 )
