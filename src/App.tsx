3a3865548905f7357a3a29e73a0a85f55aa4a85c
Winiicius <winiciusalexandre@gmail.com>
2026-02-12 12:11:59 -0300

    feat: wire products routes in App

--- src/App.tsx
+++ src/App.tsx
@@ -7,13 +7,13 @@ import { ProductsListPage } from "./features/products/pages/ProductsListPage";
 import { RawMaterialsListPage } from "./features/raw-materials/pages/RawMaterialsListPage";
 import { RawMaterialFormPage } from "./features/raw-materials/pages/RawMaterialFormPage";
 import { ProductionPage } from "./features/production/pages/ProductionPage";
+import { ProductFormPage } from "./features/products/pages/ProductFormPage";
 
 export default function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/login" element={<LoginPage />} />
-
                 <Route
                     element={
                         <PrivateRoute>
@@ -26,6 +26,8 @@ export default function App() {
                     <Route path="/products" element={<ProductsListPage />} />
                     <Route path="/raw-materials" element={<RawMaterialsListPage />} />
                     <Route path="/production" element={<ProductionPage />} />
+                    <Route path="/products/new" element={<ProductFormPage />} />
+                    <Route path="/products/:id" element={<ProductFormPage />} />
                     <Route path="/raw-materials/:id" element={<RawMaterialFormPage />} />
                     <Route path="/raw-materials/new" element={<RawMaterialFormPage />} />
 
